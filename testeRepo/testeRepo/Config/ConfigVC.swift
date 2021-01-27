2a6564285011ea88599a50d93d5a4dc0aac65504
Caio Fabrini <caiofabrini@outlook.com>
2021-01-27 20:16:02 -0300

    criei respectivos files para meu app

---
+++ testeRepo/testeRepo/Config/ConfigVC.swift
@@ -0,0 +1,17 @@
+//
+//  ConfigVC.swift
+//  testeRepo
+//
+//  Created by Caio on 27/01/21.
+//
+
+import Foundation
+import UIKit
+
+class ConfigVC:UIViewController{
+    
+    
+    
+    
+    
+}
---
+++ testeRepo/testeRepo/MyData/MyDataVC.swift
@@ -0,0 +1,18 @@
+//
+//  MyData.swift
+//  testeRepo
+//
+//  Created by Caio on 27/01/21.
+//
+
+import Foundation
+import UIKit
+
+class MyDataVC:UIViewController{
+    
+    
+    
+    
+    
+    
+}
